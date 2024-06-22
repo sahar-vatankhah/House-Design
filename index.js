e16bc8efe8ac0c5887d2ef960fe98b351d22b19c
sahar vatankhah <Svatankhah50@gmail.com>
2024-06-21 23:14:34 -0700

    add responsive and image

--- index.js
+++ index.js
@@ -1,14 +1,128 @@
 const panels = document.querySelectorAll(".panel");
 
-panels.forEach((panel) => {
-  panel.addEventListener("click", () => {
+panels.forEach((item) => {
+  item.addEventListener("click", () => {
     removeActiveClasses();
-    panel.classList.add("active");
+    item.classList.add("active");
   });
 });
 
 const removeActiveClasses = () => {
-  panels.forEach((panel) => {
-    panel.classList.remove("active");
+  panels.forEach((item) => {
+    item.classList.remove("active");
   });
 };
+
+//////////////////////////////////////
+
+const sliderContainer = document.querySelector('.slider-container');
+
+const leftSlideContent = [
+  { title: 'Lighting', Details: 'new collection', color: '#b99f8a' },
+  { title: 'Textiles', Details: 'new collection', color: '#354f32' },
+  { title: 'Flooring', Details: 'new collection', color: '#657e85' },
+  { title: 'Furniture', Details: 'new collection', color: '#2b2e32' }
+];
+
+const createSlideSection = () => {
+  const leftSlide = document.createElement('div');
+  leftSlide.classList.add('left-slide');
+  
+  leftSlideContent.forEach((item, index) => {
+    const div = document.createElement('div');
+    div.classList.add(`par${index + 1}`);
+    div.style.backgroundColor = item.color;
+    leftSlide.append(div);
+    
+    const h1 = document.createElement('h2');
+    h1.textContent = item.title;
+    div.append(h1);
+    
+    const p = document.createElement('p');
+    p.textContent = item.Details;
+    div.append(p);
+  });
+  
+  return leftSlide;
+};
+
+const createSlideImages = async () => {
+  const rightSlide = document.createElement('div');
+  rightSlide.classList.add('right-slide');
+  
+  const urls = await fetchImages();
+  
+  urls.forEach(url => {
+    const div = document.createElement('div');
+    div.style.backgroundImage = `url(${url})`;
+    div.classList.add('slide');
+    rightSlide.appendChild(div);
+  });
+  
+  return rightSlide;
+};
+
+const keyApi = '2Y8SgEv2Rp85Lh7SLCAEDIdTgYKa-d4Hj_Q5NJr0Acg';
+const query = 'home design';
+const apiUrl = `https://api.unsplash.com/search/photos?query=${query}&client_id=${keyApi}&per_page=4`;
+
+const fetchImages = async () => {
+  const response = await fetch(apiUrl);
+  const data = await response.json();
+  return data.results.map(photo => photo.urls.regular);
+};
+
+const createButtons = () => {
+  const actionButtons = document.createElement('div');
+  actionButtons.classList.add('action-buttons');
+  
+  const downButton = document.createElement('button');
+  downButton.classList.add('down-button');
+  const icon1 = document.createElement('i');
+  icon1.classList.add('bi','bi-arrow-down');
+  downButton.append(icon1);
+  actionButtons.append(downButton);
+  
+  const upButton = document.createElement('button');
+  upButton.classList.add('up-button');
+  const icon = document.createElement('i');
+  icon.classList.add('bi','bi-arrow-up');
+  upButton.append(icon);
+  actionButtons.append(upButton);
+  
+  return actionButtons;
+};
+
+const initializeSlider = async () => {
+  sliderContainer.append(createSlideSection());
+  sliderContainer.append(await createSlideImages());
+  sliderContainer.append(createButtons());
+
+  const slideRight = document.querySelector('.right-slide');
+  const slideLeft = document.querySelector('.left-slide');
+  const upButton = document.querySelector('.up-button');
+  const downButton = document.querySelector('.down-button');
+  const slidesLength = slideRight.querySelectorAll('div').length;
+
+  let activeSlideIndex = 0;
+
+  slideLeft.classList.add(`top-${slidesLength - 1}`);
+
+  const changeSlide = (direction) => {
+    const sliderHeight = sliderContainer.clientHeight;
+    if (direction === "up") {
+      activeSlideIndex++;
+      if (activeSlideIndex > slidesLength - 1) activeSlideIndex = 0;
+    } else if (direction === "down") {
+      activeSlideIndex--;
+      if (activeSlideIndex < 0) activeSlideIndex = slidesLength - 1;
+    }
+    slideRight.style.transform = `translateY(-${activeSlideIndex * sliderHeight}px)`;
+    slideLeft.style.transform = `translateY(${activeSlideIndex * sliderHeight}px)`;
+  };
+
+  upButton.addEventListener("click", () => changeSlide("up"));
+  downButton.addEventListener("click", () => changeSlide("down"));
+};
+
+initializeSlider();
--- signup.js
+++ signup.js
@@ -1,109 +1,109 @@
-import { Authentication } from './utils/auth.js';
-import { LocalStorageData } from './utils/storage.js';
-// class LocalStorageData {
-//   static setData(data) {
-//     localStorage.setItem('user', JSON.stringify(data._id));
-//   }
+// import { Authentication } from './utils/auth.js';
+// import { LocalStorageData } from './utils/storage.js';
+class LocalStorageData {
+  static setData(data) {
+    localStorage.setItem('user', JSON.stringify(data._id));
+  }
 
-//   static getData(term) {
-//     const data = localStorage.getItem(term);
-//     return JSON.parse(data);
-//   }
+  static getData(term) {
+    const data = localStorage.getItem(term);
+    return JSON.parse(data);
+  }
 
-//   static removeData() {
-//     localStorage.removeItem('user');
-//   }
-// }
+  static removeData() {
+    localStorage.removeItem('user');
+  }
+}
 
 
 // ////////////
-// const BASE_URL = 'https://66695a342e964a6dfed4c586.mockapi.io';
+const BASE_URL = 'https://66695a342e964a6dfed4c586.mockapi.io';
 
-// class Authentication {
-// constructor(email, password) {
-//   this.email = email;
-//   this.password = password;
-// }
+class Authentication {
+constructor(email, password) {
+  this.email = email;
+  this.password = password;
+}
 
-// verifyData() {
-//   if (!this.email.endsWith('@gmail.com')) {
-//     throw new Error('emailError');
-//   }
+verifyData() {
+  if (!this.email.endsWith('@gmail.com')) {
+    throw new Error('emailError');
+  }
 
-//   if (this.password.length < 5) {
-//     throw new Error('passwordError');
-//   }
-// }
+  if (this.password.length < 5) {
+    throw new Error('passwordError');
+  }
+}
 
-// async signup() {
-//   try {
-//     const getUsers = await fetch(`${BASE_URL}/users`);
-//     const users = await getUsers.json();
+async signup() {
+  try {
+    const getUsers = await fetch(`${BASE_URL}/users`);
+    const users = await getUsers.json();
 
 
-//     const doesUserExists = users.find((user) => user.email === this.email);
+    const doesUserExists = users.find((user) => user.email === this.email);
 
-//     if (doesUserExists) {
-//       return {
-//         statusCode: 409,
-//         message: 'the email is already exists',
-//         data: '',
-//       };
-//     }
+    if (doesUserExists) {
+      return {
+        statusCode: 409,
+        message: 'the email is already exists',
+        data: '',
+      };
+    }
 
-//     const response = await fetch(`${BASE_URL}/users`, {
-//       method: 'POST',
-//       body: JSON.stringify({
-//         email: this.email,
-//         password: this.password,
-//       }),
-//       headers: {
-//         'content-type': 'application/json',
-//       },
-//     });
+    const response = await fetch(`${BASE_URL}/users`, {
+      method: 'POST',
+      body: JSON.stringify({
+        email: this.email,
+        password: this.password,
+      }),
+      headers: {
+        'content-type': 'application/json',
+      },
+    });
 
-//     const createdUser = await response.json();
+    const createdUser = await response.json();
 
-//     return {
-//       statusCode: 201,
-//       message: 'signed up successfully',
-//       data: createdUser,
-//     };
-//   } catch (error) {
-//     console.log('in catch block');
-//     console.log(error);
-//   }
-// }
+    return {
+      statusCode: 201,
+      message: 'signed up successfully',
+      data: createdUser,
+    };
+  } catch (error) {
+    console.log('in catch block');
+    console.log(error);
+  }
+}
 
-// async signin() {
-//   const getUsers = await fetch(`${BASE_URL}/users`);
-//   const users = await getUsers.json();
+async signin() {
+  const getUsers = await fetch(`${BASE_URL}/users`);
+  const users = await getUsers.json();
 
-//   const doesUserExits = users.find((user) => user.email === this.email);
+  const doesUserExits = users.find((user) => user.email === this.email);
 
-//   if (!doesUserExits) {
-//     return {
-//       statusCode: 404,
-//       message: 'email or password incorrect',
-//       data: '',
-//     };
-//   }
+  if (!doesUserExits) {
+    return {
+      statusCode: 404,
+      message: 'email or password incorrect',
+      data: '',
+    };
+  }
 
-//   if (this.password !== doesUserExits.password) {
-//     return {
-//       statusCode: 404,
-//       message: 'email or password incorrect',
-//       data: '',
-//     };
-//   }
+  if (this.password !== doesUserExits.password) {
+    return {
+      statusCode: 404,
+      message: 'email or password incorrect',
+      data: '',
+    };
+  }
 
-//   return {
-//     statusCode: 200,
-//     message: 'logged in successfully',
-//     data: doesUserExits,
-//   };
-// }
-// }
+  return {
+    statusCode: 200,
+    message: 'logged in successfully',
+    data: doesUserExits,
+  };
+}
+}
 //////////
 
 
